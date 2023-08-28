d52bce55c7f89f5fa91ddd1e9d3063812f27d57f
maina-david <waihura.m@gmail.com>
2023-08-28 23:46:30 +0300

    add pending elevator call model and queue logic

--- backend/controllers/BuildingController.ts
+++ backend/controllers/BuildingController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import Building from '../models/Building'
 import Elevator from '../models/Elevator'
 import ElevatorLog from '../models/ElevatorLog'
+import PendingElevatorCall from '../models/PendingElevatorCall'
 
 export const createBuilding = async (req: Request, res: Response) => {
     const { name, number_of_floors, active, elevators } = req.body
@@ -99,22 +100,29 @@ export const callElevator = async (req: Request, res: Response) => {
 
             if (elevatorStatus !== "idle") {
                 // If elevator is not idle, queue the call
-                // Implement your queue logic here
+                await PendingElevatorCall.create({
+                    ElevatorId: elevatorId,
+                    targetFloor,
+                    executed: false
+                })
+
             } else {
                 // Elevator is idle, process the call
-                // Log the elevator call
+                // Log the elevator call with the current elevator state awaiting processing
                 const newLogEntry = await ElevatorLog.create({
                     currentFloor: latestElevatorLog.currentFloor,
-                    state: "moving",
+                    state: latestElevatorLog.state,
                     direction: targetFloor > latestElevatorLog.currentFloor ? "up" : "down",
                     action: "call",
                     details: `Called to floor ${targetFloor}`,
                     ElevatorId: elevatorId,
                 })
-
-                // Return a success response
-                res.status(200).json({ message: 'Elevator call processed successfully' })
+                // Process elevator call
+                await moveElevatorToFloor(newLogEntry, targetFloor)
             }
+
+            // Return a success response
+            res.status(200).json({ message: 'Elevator call processed successfully' })
         } else {
             // Handle case where there are no elevator logs
             // This might be the initial state of the elevator
@@ -124,4 +132,78 @@ export const callElevator = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Internal server error' })
     }
 }
+
+const moveElevatorToFloor = async (logEntry: ElevatorLog, targetFloor: number) => {
+    try {
+        const elevator = await Elevator.findByPk(logEntry.ElevatorId)
+
+        if (!elevator) {
+            console.error('Elevator not found')
+            return
+        }
+
+        const currentFloor = logEntry.currentFloor
+        const direction = currentFloor < targetFloor ? 'MovingUp' : 'MovingDown'
+        const movementStep = currentFloor < targetFloor ? 1 : -1
+
+        // Simulate elevator movement
+        await simulateMovement(elevator.id, currentFloor, targetFloor, direction, movementStep)
+
+        // Elevator reached the target floor, log and perform actions
+        await simulateAction(elevator.id, targetFloor, 'Stopped', 'stop', `Elevator reached target floor ${targetFloor}`)
+
+        // Simulate door actions
+        const doorActions = [
+            ['doorsOpening', 'open_doors', 'Doors opening'],
+            ['doorsOpen', 'doors_opened', 'Doors opened'],
+            ['doorsClosing', 'close_doors', 'Doors closing'],
+            ['doorsClosed', 'doors_closed', 'Doors closed']
+        ]
+
+        for (const [state, action, details] of doorActions) {
+            await simulateAction(elevator.id, targetFloor, state, action, details, 2000)
+        }
+
+        // Elevator returns to idle state
+        await simulateAction(elevator.id, targetFloor, 'idle', 'idle', 'Elevator returned to idle state')
+    } catch (error) {
+        console.error('Error moving elevator:', error)
+    }
+}
+
+const simulateMovement = async (elevatorId: number, currentFloor: number, targetFloor: number, direction: string, movementStep: number) => {
+    while (currentFloor !== targetFloor) {
+        await sleep(5000) // Simulate 5 seconds of movement time
+
+        await ElevatorLog.create({
+            ElevatorId: elevatorId,
+            currentFloor,
+            state: direction,
+            direction,
+            action: 'move',
+            details: `Moving ${direction} to floor ${currentFloor + movementStep}`,
+        })
+
+        currentFloor += movementStep
+    }
+}
+
+const simulateAction = async (elevatorId: number, currentFloor: number, state: string, action: string, details: string, delay = 0) => {
+    if (delay > 0) {
+        await sleep(delay)
+    }
+
+    await ElevatorLog.create({
+        ElevatorId: elevatorId,
+        currentFloor,
+        state,
+        direction: null,
+        action,
+        details,
+    })
+}
+
+// Simulate sleep using promises
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
 
